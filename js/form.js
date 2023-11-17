5066dc598887e07fa55871e83d3520486ad9a082
webLana <148962133+webLana@users.noreply.github.com>
2023-11-17 16:36:07 +0200

    Update form.js
    last

--- js/form.js
+++ js/form.js
@@ -4,43 +4,47 @@ const goToFormButton = document.querySelector('#go-to-form-btn');
 const userEmailField = document.querySelector('#user-email');
 
 
+
 goToFormButton.addEventListener('click', function (e) {
     e.preventDefault();
     form.scrollIntoView();
 });
 
 function clearFormFields() {
-    const modalFiends = formModal.querySelectorAll('input');
+    const fieldName = form.querySelector('input[type="text"]');
+    const fieldEmail = form.querySelector('input[type="email"]');
 
-    modalFiends.forEach( field => { 
-        field.value = ''
-    });
+    fieldName.value = '';
+    fieldEmail.value = '';
 }
 
+function addGooseElement() {
+    const targetContainer = document.querySelector('#form');
+    const gooseEl = document.createElement('img');
+    gooseEl.classList.add('gus-anim');
+
+    targetContainer.appendChild(gooseEl);
+}
 
 function showGooseAnim() {
-    const targetContainer = document.querySelector('#form');
-    const gusImage = document.createElement('img');
-    gusImage.setAttribute('src', './img/gus-anim.gif');
-    gusImage.classList.add('gus-anim');
+    const gooseEl = document.querySelector('.gus-anim');
 
-    targetContainer.appendChild(gusImage);
+    gooseEl.setAttribute('src', './img/gus-anim.gif');
+    
 
-    setTimeout(2000, () => {
-        targetContainer.removeChild(gusImage);
-    })
+    setTimeout(() => {
+        gooseEl.removeAttribute('src');
+    }, 4000)
 }
 
+addGooseElement();
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     const formData = new FormData(form);
 
-    launchBtn.setAttribute('disabled', true)
-
-    if (userEmailField?.value?.length > 30) {
-        return;
-    }
+    launchBtn.setAttribute('disabled', true);
+    launchBtn.style.opacity = '0.7';
 
     fetch("/", {
       method: "POST",
@@ -50,11 +54,16 @@ form.addEventListener('submit', e => {
       .then(() => {
         showGooseAnim();
 
+        launchBtn.removeAttribute('disabled')
+        clearFormFields();
+
+
         setTimeout(() => {
-            // launchBtn.removeAttribute('disabled')
-            // clearFormFields();
-        }, 2000);
+            launchBtn.style.opacity = '1';
+        }, 4000)
       })
       .catch((error) => console.log('Sending form failed'));
 })
+
+
 
